1578ea9583c04547432bd19a9041ad41c01472de
David Curdie <curdog4@curdie.us>
2020-03-03 17:04:05 -0800

    add workflow info from snakemake

---
+++ snakemake/wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+snakemake --cluster-config cluster.json \
+          --cluster "sbatch -N {cluster.nodes} --mem={cluster.memory} --cpus-per-task={cluster.ncpus} --parsable -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error}" \
+          --cluster-status ./scripts/status.py \
+          --use-conda \
+          --use-singularity \
+          --cores 8 \
+          $@
+
