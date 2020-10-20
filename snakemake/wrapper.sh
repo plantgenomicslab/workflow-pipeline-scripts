56cbd042fcaf986033abd35dff710af747dc6086
Won Cheol Yim <wyim@unr.edu>
2020-10-20 15:32:38 -0700

    Update wrapper.sh

--- snakemake/wrapper.sh
+++ snakemake/wrapper.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+set -x
+set -e
 snakemake --cluster-config cluster.json \
           --cluster "sbatch -N {cluster.nodes} --mem={cluster.memory} --cpus-per-task={cluster.ncpus} --parsable -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error}" \
-          --cluster-status ./scripts/status.py \
-          --use-conda \
-          --use-singularity \
-          --cores 8 \
+          --cluster-status ./src/status.py \
+          --max-jobs-per-second 5 \
+          --max-status-checks-per-second 3 \
+          --jobs 444450 \
+          --latency-wait 440 \
+          --notemp \
           "$@"
-
